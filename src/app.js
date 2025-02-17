8b333d69c3c5d9a3e27f67ec7955974edf12da17
xtrmcdr <83060894+xtrmcdr@users.noreply.github.com>
2025-02-17 23:49:18 +0800

    update

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const app = express();
 app.get("/", (req, res, next) => {
   return res.status(200).json({
     message: "Hello orang kampung dari root!",
+    DEBUG: process.env.DEBUG === 1 || `${process.env.DEBUG}` === `1`,
+    DATABASE_URL: process.env.DATABASE_URL
+      ? process.env.DATABASE_URL
+      : "not here",
   });
 });
 
@@ -20,8 +24,8 @@ app.use((req, res, next) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("listening on port http://localhost:3000");
-});
+// app.listen(3000, () => {
+//   console.log("listening on port http://localhost:3000");
+// });
 
 exports.handler = serverless(app);
